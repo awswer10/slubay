9ab1046c454d2391866347eb2bcaecb5651e7541
Son Vuong <hson94@gmail.com>
2014-04-20 22:53:07 -0400

    Add posts and messages models. However, haven't write unit test for them. Need to write unit tests and change up some functions after discussion.

---
+++ Code/models/messages.js
@@ -0,0 +1,84 @@
+// A model for a visitor collection
+var mongojs = require('mongojs');
+var bcrypt = require('bcrypt');
+
+// Access the database
+var db = mongojs('slubay', ['messages']);
+
+// Create a new message
+module.exports.create = function(sender,receiver,title,message,date,callback) {
+    
+   
+    if (error) throw error;
+        
+    db.messages.findAndModify({
+        query: {_id:_id},
+        update: {$setOnInsert:{sender:sender,receiver:receiver,title:title,message:message,date:date}},
+            new: true,
+            upsert: true
+            
+        }, function(error, message) {
+            if (error) throw error;
+            
+            callback(_id);
+        });
+};
+
+
+
+
+//Retrieve one message
+module.exports.retrieve = function(_id, callback) {
+    
+    db.messages.findOne({_id:_id}, function(error,message) {
+        if (error) throw error;
+       
+        if (!message) {
+            callback(false);
+        }
+        else{
+            callback(message);
+        }
+    });
+};
+
+//count number of message in the database
+module.exports.count=function(callback){
+  db.messages.count(function(error,count){
+        if (error) throw error;
+        callback(count);
+  });
+};
+//delete a message
+module.exports.delete=function(_id,callback){
+     db.messages.remove({_id:_id}, function(error) {
+        if (error) throw error;
+        callback(true);
+     });
+     
+}
+
+//delete all messages in an user(receiver) inbox
+module.exports.deleteuser=function(receiver,callback){
+     db.messages.remove({receiver:receiver}, function(error) {
+        if (error) throw error;
+        callback(true);
+     });
+     
+}
+
+// Delete all messages
+module.exports.deleteAll = function(callback) {
+    db.messages.remove({}, function(error) {
+        if (error) throw error;
+        callback();
+    });
+};
+
+// Close the connection
+module.messages.close = function(callback) {
+    db.close(function(error) {
+        if (error) throw error;
+        callback();
+    });
+}
---
+++ Code/models/posts.js
@@ -0,0 +1,131 @@
+// A model for a visitor collection
+var mongojs = require('mongojs');
+var bcrypt = require('bcrypt');
+
+// Access the database
+var db = mongojs('slubay', ['posts']);
+
+// Create a new post
+module.exports.create = function(user,title,category,description,date,views,comments,callback) {
+    
+   
+    if (error) throw error;
+        
+    db.posts.findAndModify({
+        query: {_id:_id},
+        update: {$setOnInsert:{user:user,title:title,category:category,date:date,views:views,
+        comments:comments}},
+            new: true,
+            upsert: true
+            
+        }, function(error, post) {
+            if (error) throw error;
+            
+            callback(_id);
+        });
+};
+
+//Edit tittle
+module.exports.editTitle = function(_id, newtitle, callback) {
+     db.posts.update({_id:_id}, {$set:{tittle:newtitle}}, function(error) {
+        if (error) throw error;
+        callback(true);
+    });
+}
+
+//Edit category
+module.exports.editCategory = function(_id, newCategory, callback) {
+     db.posts.update({_id:_id}, {$set:{category:newCategory}}, function(error) {
+        if (error) throw error;
+        callback(true);
+    });
+}
+
+//Edit description
+module.exports.editDescription = function(_id, newDescription, callback) {
+     db.posts.update({_id:_id}, {$set:{description:newDescription}}, function(error) {
+        if (error) throw error;
+        callback(true);
+    });
+}
+
+//Edit date
+module.exports.editDate = function(_id, newDate, callback) {
+     db.posts.update({_id:_id}, {$set:{date:newDate}}, function(error) {
+        if (error) throw error;
+        callback(true);
+    });
+}
+
+
+//Edit views
+module.exports.editViews = function(_id, newviews, callback) {
+     db.posts.update({_id:_id}, {$set:{views:newviews}}, function(error) {
+        if (error) throw error;
+        callback(true);
+    });
+}
+
+//Retrieve all post
+module.exports.retrieveAll = function(callback) {
+    
+    db.posts.find({}, function(error,posts) {
+        if (error) throw error;
+        callback(posts);
+    });
+};
+
+//Retrieve one post
+module.exports.retrieve = function(_id, callback) {
+    
+    db.posts.findOne({_id:_id}, function(error,post) {
+        if (error) throw error;
+       
+        if (!post) {
+            callback(false);
+        }
+        else{
+            callback(post);
+        }
+    });
+};
+
+//count number of post in the database
+module.exports.count=function(callback){
+  db.posts.count(function(error,count){
+        if (error) throw error;
+        callback(count);
+  });
+};
+//delete a post
+module.exports.delete=function(_id,callback){
+     db.posts.remove({_id:_id}, function(error) {
+        if (error) throw error;
+        callback(true);
+     });
+     
+}
+
+//delete all post from one user
+module.exports.deleteuser=function(user,callback){
+     db.posts.remove({user:user}, function(error) {
+        if (error) throw error;
+        callback(true);
+     });
+     
+}
+// Delete all posts
+module.exports.deleteAll = function(callback) {
+    db.posts.remove({}, function(error) {
+        if (error) throw error;
+        callback();
+    });
+};
+
+// Close the connection
+module.exports.close = function(callback) {
+    db.close(function(error) {
+        if (error) throw error;
+        callback();
+    });
+}
