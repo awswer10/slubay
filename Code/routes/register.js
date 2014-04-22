b5fd32f07d2f83248415a19b331638d6f659b4c1
Austin Swertfager <austin.swertfager@gmail.com>
2014-04-22 13:36:44 -0400

    Edited register route to include new fields

--- Code/routes/register.js
+++ Code/routes/register.js
@@ -5,12 +5,21 @@ var validator = require('validator');
 module.exports = function(request,response) {
    
     var name = validator.escape(request.body.name);
+    var realname = validator.escape(request.body.realname);
+    var email = validator.escape(request.body.email);
+    var year = validator.escape(request.body.year);
+    var major = validator.escape(request.body.major);
     var password = validator.escape(request.body.password);
     
-    users.create(name, password, function(success) {
+    users.create(name, realname, password, email, year, major,  function(success) {
         
         if (success) {
             request.session.username = name;
+            request.session.realname = realname;
+            request.session.password = password;
+            request.session.email = email;
+            request.session.year = year;
+            request.session.major = major;
             response.redirect('/home');
         }
         
