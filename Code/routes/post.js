a432c5e04c594b23963cdfb1b134fde717a673a9
Jacob Hurlbut <jake@jakehurlbut.org>
2014-05-05 19:53:18 -0400

    Added contact options to the post page.

--- Code/routes/post.js
+++ Code/routes/post.js
@@ -8,61 +8,56 @@ module.exports = function(request, response) {
 	var postid = url.substring(index + 1);
 	var index0 = url.lastIndexOf("home/");
 	var categoryid = url.substring(index0 + 5, index);
-   
+
 	var username = request.session.username;
-        var editDelete;
+	var editDelete;
 
 	if (username) {
-                  try {
-                          
-                           
-                           
-                     
-                        posts.increaseViews(postid,function(){});
+		try {
+			posts.increaseViews(postid, function() {});
 			posts.retrieve(postid, function(post) {
-                              users.admin(username, function(success) {
-                                 
-                                 var dateObj = new Date();
-                                 var month = dateObj.getUTCMonth();
-                                 var day = dateObj.getUTCDate();
-                                 var year = dateObj.getUTCFullYear();
-                                 var date = month + "/" + day + "/" + year;
-                                 post.date = date;
-                                 var editDelete;
-                                 if (success) {
-                                      editDelete=true;
-                                   } else {
-                                      posts.retrieve(postid,function(post) {
-                                         if (username == post.user) {
-                                            editDelete=true;
-                                         } else {
-                                            editDelete=false;
-                                         }
-                                      });
-                                   }
- 
- 
-                                 comments.retrievePostid(postid, function(comments) {
-					users.retrieveEmail(username,function(email){
-						response.render('post', {
-						      username: username,
-						      categoryid: categoryid,
-						      postid: postid,
-						      post: post,
-						      comments: comments,
-						      editDelete:editDelete,
-						      email:email
-						     
-					      });
+				users.admin(username, function(success) {
+
+					var dateObj = new Date();
+					var month = dateObj.getUTCMonth();
+					var day = dateObj.getUTCDate();
+					var year = dateObj.getUTCFullYear();
+					var date = month + "/" + day + "/" + year;
+					post.date = date;
+					var editDelete;
+					if (success) {
+						editDelete = true;
+					} else {
+						posts.retrieve(postid, function(post) {
+							if (username == post.user) {
+								editDelete = true;
+							} else {
+								editDelete = false;
+							}
+						});
+					}
+
+					comments.retrievePostid(postid, function(comments) {
+						users.retrieveEmail(post.user, function(email) {
+							response.render('post', {
+								username: username,
+								categoryid: categoryid,
+								postid: postid,
+								post: post,
+								comments: comments,
+								editDelete: editDelete,
+								email: email
+
+							});
+						});
 					});
-                                 });
-                              });   
+				});
 
 			});
-		} catch(e) {
+		} catch (e) {
 			response.render('default');
 		}
-		
+
 	} else {
 		response.redirect("/");
 		delete request.session.error;
