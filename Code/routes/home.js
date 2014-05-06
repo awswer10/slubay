d7488413e4ef6a85fb195e870eaaaac3f71f935e
Jacob Hurlbut <jake@jakehurlbut.org>
2014-05-05 20:22:08 -0400

    Eliminated the "no post crash" and added a nice time badge to the recent posts. (Requires moment.js)

--- Code/routes/home.js
+++ Code/routes/home.js
@@ -1,15 +1,16 @@
 // Home page
 var categories = require('../models/categories');
 var posts = require('../models/posts');
+var moment = require('moment');
 
 module.exports = function(request, response) {
 
 	var username = request.session.username;
 	var error = request.session.error;
 
-        // Displays home page with list of all categories, as well as
-        // a list of the most recent posts.  If no user is logged in,
-        // they are redirected to login.
+	// Displays home page with list of all categories, as well as
+	// a list of the most recent posts.  If no user is logged in,
+	// they are redirected to login.
 	if (username && !error) {
 
 		var cats = null;
@@ -17,42 +18,52 @@ module.exports = function(request, response) {
 
 		categories.retrieveAll(function(catArray) {
 			var catLoopCount = catArray.length;
-			catArray.forEach(function(category) {
-				posts.countCategory(category.name, function(count) {
-					category.count = count;
-					catLoopCount--;
-					console.log(catLoopCount);
-					if (catLoopCount === 0) {
-						cats = catArray;
-						finished();
-					}
+
+			if (catLoopCount === 0) {
+				cats = [];
+				finished();
+			} else {
+				catArray.forEach(function(category) {
+					posts.countCategory(category.name, function(count) {
+						category.count = count;
+						catLoopCount--;
+						console.log(catLoopCount);
+						if (catLoopCount === 0) {
+							cats = catArray;
+							finished();
+						}
+					});
 				});
-			});
-			
-			
+			}
 		});
 
-                // function that returns the most recent posts added
-                // to the database
+		// function that returns the most recent posts added
+		// to the database
 		posts.retrieveRecent(function(recentPosts) {
-			
+
 			var rpLoopCount = recentPosts.length;
-			recentPosts.forEach(function(post) {
-				categories.retrieveID(post.category, function(catID) {
-					post.category = catID;
-					rpLoopCount--;
-					if (rpLoopCount === 0) {
-						rePo = recentPosts;
-						finished();
-					}
+
+			if (rpLoopCount === 0) {
+				rePo = [];
+				finished();
+			} else {
+				recentPosts.forEach(function(post) {
+					categories.retrieveID(post.category, function(catID) {
+						post.category = catID;
+						post.date = moment(post.date).fromNow()
+						rpLoopCount--;
+						if (rpLoopCount === 0) {
+							rePo = recentPosts;
+							finished();
+						}
+					});
 				});
-			});
-			
-			
+			}
 		});
 
-                // Once categories and recent posts are retrieved,
-                // home page is rendered.
+		// Once categories and recent posts are retrieved,
+		// home page is rendered.
+
 		function finished() {
 			if (cats !== null && rePo !== null) {
 				console.log("render");
@@ -60,13 +71,13 @@ module.exports = function(request, response) {
 					username: username,
 					categories: cats,
 					recentPosts: rePo,
-                                        error:request.session.error1
+					error: request.session.error1
 				});
-                                delete request.session.error1;
+				delete request.session.error1;
 			}
-                        //else {
-                        //    response.render('home', {username:username,categories:cats});
-                        //}
+			//else {
+			//    response.render('home', {username:username,categories:cats});
+			//}
 		}
 
 
