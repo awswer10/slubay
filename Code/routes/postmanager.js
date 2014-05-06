c00340240ea91da364eacb836b3d68a25242a7ef
Jacob Hurlbut <jake@jakehurlbut.org>
2014-05-05 21:42:16 -0400

    Styled user posts page

--- Code/routes/postmanager.js
+++ Code/routes/postmanager.js
@@ -1,22 +1,29 @@
 // Post manager page
 var posts = require('../models/posts');
+var moment = require('moment');
 
 module.exports = function(request, response) {
-    
-   var username = request.session.username;
-   
-   // Retrieves all post from individual user, and displays.
-   // If no user is logged in, login is rendered.
-   if (username) {
-      posts.retrieveUser(username, function(posts){
-         response.render('postmanager', {username:username,posts:posts});
-      });
-      
-   }
-   
-   else {
-        response.render('login', {error:request.session.error});
-        delete request.session.error;
-        response.redirect('/');
-   }
-};
+
+	var username = request.session.username;
+
+	// Retrieves all post from individual user, and displays.
+	// If no user is logged in, login is rendered.
+	if (username) {
+		posts.retrieveUser(username, function(posts) {
+				posts.forEach(function(post) {
+						post.date = moment(post.date).fromNow();
+					});
+					response.render('postmanager', {
+						username: username,
+						posts: posts
+					});
+				});
+
+		} else {
+			response.render('login', {
+				error: request.session.error
+			});
+			delete request.session.error;
+			response.redirect('/');
+		}
+	};
